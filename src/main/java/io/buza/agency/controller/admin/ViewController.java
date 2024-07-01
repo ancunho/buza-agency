66a73611009d60d41184a0d4d98e45865b884a76
cunho <ancunho@naver.com>
2024-07-01 23:21:00 +0900

    #5 post관련 화면 view link추가

--- src/main/java/io/buza/agency/controller/admin/ViewController.java
+++ src/main/java/io/buza/agency/controller/admin/ViewController.java
@@ -21,19 +21,70 @@ public class ViewController {
         return "index";
     }
 
+    /**
+     * 관리자 로그인 화면
+     */
     @GetMapping("/login")
     public String admin_login() {
         return "login";
     }
 
+    /**
+     * 회원 리스트 화면
+     */
     @GetMapping("/account")
     public String account_list() {
         return "account/list";
     }
 
-    @GetMapping("/account/create_new")
-    public String account_create_new() {
-        return "account/create_new";
+    /**
+     * 회원 신규 생성 화면
+     */
+    @GetMapping("/account/create")
+    public String account_create() {
+        return "account/create";
+    }
+
+    /**
+     * 포스트 리스트 화면
+     */
+    @GetMapping("/post")
+    public String post_list() {
+        return "post/list";
+    }
+
+    /**
+     * 포스트 신규 생성 화면
+     */
+    @GetMapping("/post/create")
+    public String post_create() {
+        return "post/create";
+    }
+
+    /**
+     * 포스트 편집 화면
+     */
+    @GetMapping("/post/modify")
+    public String post_modify() {
+        return "post/modify";
+    }
+
+    /**
+     * 포스트 - 카테고리 관리
+     * 리스트/편집 All in one page
+     */
+    @GetMapping("/post/category")
+    public String post_category_management() {
+        return "post/category";
+    }
+
+    /**
+     * 포스트 - 태그 관리
+     * 리스트/편집 All in one page
+     */
+    @GetMapping("/post/tag")
+    public String post_tag_management() {
+        return "post/tag";
     }
 
 
